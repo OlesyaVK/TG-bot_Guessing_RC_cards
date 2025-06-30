a6fa491f6b277f9a968ab178da690984d3d08363
OlesyaVK <160909454+OlesyaVK@users.noreply.github.com>
2025-06-30 17:33:24 +0300

    Return main.py to previous version

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.strategy import FSMStrategy
 from dotenv import load_dotenv
-from TG-bot_Guessing_RC_cards.handlers import register_handlers
+from handlers import register_handlers
 
 load_dotenv()
 
